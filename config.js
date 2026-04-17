4e6236cdad481216107dc0c5fe4ee00cc5ae8f2a
prince <raymondpaul91167@gmail.com>
2026-04-17 15:38:54 +0530

    Add config with API keys

---
+++ config.js
@@ -0,0 +1,7 @@
+// TARS Configuration - Add your API keys here
+// This file is gitignored to protect your keys
+
+const CONFIG = {
+    groqApiKey: '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',
+    fishApiKey: '7d54bb5db26044d5bf4eff339abd61a7'
+};
